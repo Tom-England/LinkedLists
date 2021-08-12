afb101c18f1d663a50de38ac01697e95b42b63ed
Tom-England <tomengland6679@gmail.com>
2021-08-12 19:06:40 +0100

    Added comments to linkedlist.h

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -3,12 +3,20 @@ struct Node{
 	struct Node* next;
 };
 
+// Returns a pointer to a Node struct that has next set to NULL
 struct Node* createNode();
 
+// Appends a Node to the end of the linked list with data set to content
+// Returns a pointer to head
 struct Node* pushNode(struct Node* head, char* content);
 
+// Removes the Node at the end of the linked list and frees its memory
 void popNode(struct Node* head);
 
+// Outputs all Nodes data and next pointer
 void printList(struct Node* head);
 
+// Searches for string searchTerm in the list
+// Returns 1 for found
+// Returns 0 for not found
 int seach(struct Node* head, char* searchTerm);
