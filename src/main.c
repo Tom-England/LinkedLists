6b7a49fe9d91b93aff59ff6a87bb9c0feed3391b
Tom-England <tomengland6679@gmail.com>
2021-08-12 19:59:05 +0100

    Formatted main.c examples

--- src/main.c
+++ src/main.c
@@ -4,20 +4,35 @@
 #include <stdlib.h>
 #include "linkedlist.c"
 
+void sysmsg(char* msg){
+	printf("====================\n");
+	printf("%s\n",msg);
+	printf("====================\n");
+}
+
 int main(){
 	struct Node* head = NULL;
 	head = pushNode(head, "First");
+	
+	sysmsg("Push & Search Test");
+
 	int lim = 1000;
 	for (int i = 0; i < lim; i++){
 		head = pushNode(head, "hi");
 	}
 	printList(head);
 	if (search(head, "hi")) { printf("Found hi\n"); } else { printf("Didn't find hi\n");}
+	
+	sysmsg("Pop & Search Test");
+
 	for (int i = 0; i < lim; i++){
 		popNode(head);
 	}
 	printList(head);
 	if (search(head, "hi")) { printf("Found hi\n"); } else{ printf("Didn't find hi\n"); }
+
+	sysmsg("Insert at index test");
+
 	for (int i = 0; i < 10; i++){
 		head = pushNode(head, "hello");
 	}
